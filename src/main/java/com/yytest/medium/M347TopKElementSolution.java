552648240cf5ac574a4b283beb7d721aaaf2997a
Emma Wang <lemeiy.wang@gmail.com>
2024-10-05 22:10:35 -0400

    M 347: version1

---
+++ src/main/java/com/yytest/medium/M347TopKElementSolution.java
@@ -0,0 +1,49 @@
+package com.yytest.medium;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/**
+ * @Description 347. Top K Frequent Elements
+ * Medium
+ * Topics
+ * Companies
+ * Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [1,1,1,2,2,3], k = 2
+ * Output: [1,2]
+ * Example 2:
+ * <p>
+ * Input: nums = [1], k = 1
+ * Output: [1]
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= nums.length <= 105
+ * -104 <= nums[i] <= 104
+ * k is in the range [1, the number of unique elements in the array].
+ * It is guaranteed that the answer is unique.
+ * <p>
+ * <p>
+ * Follow up: Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+ * @Author Emma
+ * @Date 2024-09-30
+ */
+public class M347TopKElementSolution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) map.put(num, map.getOrDefault(num, 0) + 1);
+
+        return map.entrySet().stream().sorted(Map.Entry.<Integer, Integer>comparingByValue().reversed()).limit(k).mapToInt(Map.Entry::getKey).toArray();
+    }
+}
---
+++ src/test/java/com/yytest/medium/M347TopKElementSolutionTest.java
@@ -0,0 +1,33 @@
+package com.yytest.medium;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class M347TopKElementSolutionTest {
+
+    @Test
+    void topKFrequent() {
+        int[] nums = new int[]{1,1,1,2,2,3};
+        M347TopKElementSolution m347TopKElementSolution = new M347TopKElementSolution();
+        int[] result = m347TopKElementSolution.topKFrequent(nums, 2);
+
+        int[] expected = {1,2};
+        assertArrayEquals(expected, result);
+
+        int[] nums2 = new int[]{1};
+        int[] result2 = m347TopKElementSolution.topKFrequent(nums2, 1);
+        int[] expected2 = {1};
+        assertArrayEquals(expected2, result2);
+
+        int[] nums3 = new int[]{1,2};
+        int[] result3 = m347TopKElementSolution.topKFrequent(nums3, 2);
+        int[] expected3 = {1,2};
+        assertArrayEquals(expected3, result3);
+
+        int[] nums4 = new int[]{-1,-1};
+        int[] result4= m347TopKElementSolution.topKFrequent(nums4, 1);
+        int[] expected4 = {-1};
+        assertArrayEquals(expected4, result4);
+    }
+}
